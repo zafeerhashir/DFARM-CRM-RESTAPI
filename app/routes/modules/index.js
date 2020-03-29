b6ef7a231906c93ed8271bccf4198b2741c90bcc
MAC <mac@MACs-MacBook-Pro.local>
2020-03-29 23:36:09 +0500

    app.js

--- app/routes/modules/index.js
+++ app/routes/modules/index.js
@@ -1,7 +1,7 @@
 const modules = require('express').Router();
 
 
-// modules.use('/animal',require('./animal/index'));
+modules.use('/animal',require('./animal/index'));
 modules.use('/milk',require('./milk/index'));
 modules.use('/feeditem',require('./feeditem/index'));
 modules.use('/feeddate',require('./feeddate/index'));
