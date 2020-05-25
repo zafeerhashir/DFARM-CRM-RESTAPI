da78dcaf6e31ee802aea7481dc9b152dfab6e995
Zafeer Hashir <zafeer.hashir@aciano.net>
2020-05-25 19:23:58 +0500

    add animal

--- app/routes/modules/animal/addanimal.js
+++ app/routes/modules/animal/addanimal.js
@@ -2,19 +2,16 @@ const animalModel = require("../../../models/animal");
 
 module.exports = async function addAnimal(req, res, next) {
   try {
-    const findAnimal = await animalModel.find({ tag: req.body.tag });
-
+    const findAnimal = await animalModel.findOne({ tag: req.body.tag });
     if (findAnimal) {
       await res
         .status(409)
         .send({ errorMessage: "Animal already added before" });
-    } else {
-      const animal = new animalModel(req.body);
-
-      await animal.save();
-
-      await res.send(animal);
     }
+
+    const animal = new animalModel(req.body);
+    await animal.save();
+    await res.send(animal);
   } catch (err) {
     res.status(500).send(err);
   }
